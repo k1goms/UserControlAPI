882af390686eb9582de15ee184334b836c83bfdb
Kaua Gomes <kauaoficialkr@outlook.com>
2026-06-08 18:58:51 -0300

    initial setup

---
+++ src/test/java/dev/k1goms/UserControl/UserControlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.k1goms.UserControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
